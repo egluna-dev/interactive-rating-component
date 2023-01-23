3b2c7d0b16785883e9fd95d1076731083cfbd2ad
egluna-dev <egluna@live.ca>
2023-01-24 00:33:13 +1100

    Created dom handlers

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log("DOM loaded")
+    const ratingButtonsDiv = document.getElementById("ratings-bar");
+    // Dividing by two to omit nested span children
+    // Subtracting 1 to omit default empty text node
+    // const numRatingValues = (ratingButtons.childNodes.length - 1) / 2;
+    const numRatingValues = ratingButtonsDiv.childElementCount;
+
+    const ratingButtonsArr = Array.from(ratingButtonsDiv.children);
+
+    const formSubmitButton = document.getElementById("form-submit-btn");
+
+    const ratingCardDiv = document.getElementById("rating-card");
+    const formResponseCard = document.getElementById("form-response-card");
+
+    const userRating_dom = document.getElementById("user-rating");
+
+    const handleSubmit = (e) => {
+        ratingCardDiv.classList.add("hidden");
+        formResponseCard.classList.remove("hidden");
+
+        console.log("Form submitted")
+    }
+
+    console.log(ratingButtonsArr);
+    formSubmitButton.addEventListener("click", handleSubmit);
 });
